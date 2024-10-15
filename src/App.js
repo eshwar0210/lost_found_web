06e4397cfdaff2bfbb9a4fd82c3610c4747219cf
eshwar <eshwarrachakonda02@gmail.com>
2024-10-16 01:06:25 +0530

    Setting up navigation routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home';
 import Register from './components/Register';
 import Login from './components/Login';
 import Profile from './components/Profile';
 import './App.css';
 
+
+const isAuthenticated = () => {
+  return !!localStorage.getItem('authToken');  
+};
+
+
+const PrivateRoute = ({ element: Component }) => {
+  return isAuthenticated() ? Component : <Navigate to="/login" />;
+};
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
+        {/* If the user is logged in, redirect to home, else to login */}
+        <Route path="/" element={isAuthenticated() ? <Navigate to="/home" /> : <Navigate to="/login" />} />
+
+        {/* Public routes */}
         <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Protected routes */}
+        <Route path="/home" element={<PrivateRoute element={<Home />} />} />
+        <Route path="/profile" element={<PrivateRoute element={<Profile />} />} />
       </Routes>
     </Router>
   );
